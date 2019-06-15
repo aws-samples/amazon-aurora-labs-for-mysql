d30a7b420c5195475d1d2de8fd87eacbaa77d356
vladvataws <vladv@38f9d33824c1.ant.amazon.com>
2019-06-15 10:47:04 -0700

    updated loadtest script, updated bastion bootstrap

---
+++ TODO.md
@@ -0,0 +1,6 @@
+* Prerequisites: screenshots for PuTTY setup
+* Create New Cluster: walkthrough
+* Connect to Cluster: steps plus updated screenshots, fix load script that goes wonky after a long time
+* Clone: steps plus updated screenshots
+* Backtrack: steps plus updated screenshots
+* Perf Ins: steps plus updated screenshots
---
+++ scripts/loadtest.py
@@ -0,0 +1,118 @@
+"""
+Amazon Aurora Labs for MySQL
+Read load generator using multiple threads
+
+Changelog:
+2019-06-14 - Initial release
+
+Dependencies:
+none
+
+License:
+This sample code is made available under the MIT-0 license. See the LICENSE file.
+"""
+
+# Dependencies
+import sys
+import argparse
+import time
+import threading
+import _thread
+import socket
+import random
+import pymysql
+
+# Define parser
+parser = argparse.ArgumentParser()
+parser.add_argument('-e', '--endpoint', help="The database endpoint")
+parser.add_argument('-p', '--password', help="The database user password")
+parser.add_argument('-u', '--username', help="The database user name")
+parser.add_argument('-d', '--database', help="The schema (database) to use")
+parser.add_argument('-t', '--threads', help="The number of threads to use", type=int, default=24)
+args = parser.parse_args()
+
+# Global variables
+query_count = 0
+max_id = 2500000
+query_iterations = 100
+lock = threading.Lock()
+
+# Query thread
+def thread_func(endpoint, username, password, schema, max_id, iterations):
+    # Specify that query_count is a global variable
+    global query_count
+    global lock
+
+    # Loop Indefinitely
+    while True:
+        try:
+            # Resolve the endpoint
+            host = socket.gethostbyname(endpoint)
+
+            # Connect to the reader endpoint
+            conn = pymysql.connect(host=host, user=username, password=password, database=schema, autocommit=True)
+
+            # Run multiple queries per connection
+            for iter in range(iterations):
+                # Generate a random number to use as the lookup value
+                key_value = str(random.randrange(1, max_id))
+                sql_command = "SELECT * FROM sbtest1 WHERE id={0};".format(key_value)
+
+                # run query
+                with conn.cursor() as cursor:
+                    cursor.execute(sql_command)
+                    cursor.close()
+
+                # Increment the executed query count
+                with lock:
+                    query_count += 1
+
+            # Close the connection
+            conn.close()
+        except:
+            # Display any exception information
+            print(sys.exc_info()[1])
+
+
+# Progress thread
+def progress_func():
+    # Specify that query_count is a global variable
+    global query_count
+    global lock
+
+    # Start timing
+    start_time = time.time()
+    initial = True
+
+    # Loop indefinitely
+    while True:
+        if initial != True:
+            # Format an output string
+            end_time = time.time()
+            output = "Queries/sec: {0} (press Ctrl+C to quit)\r".format(int(query_count / (end_time-start_time)))
+            start_time = end_time
+
+            # Reset the executed query count
+            with lock:
+                query_count = 0
+
+            # Write to STDOUT and flush
+            sys.stdout.write(output)
+            sys.stdout.flush()
+
+            # Sleep this thread for 1 second
+            time.sleep(5)
+
+        # No longer initial pass
+        initial = False
+
+# Start progress thread
+_thread.start_new_thread(progress_func, ())
+
+# Start readers
+for thread_id in range(args.threads):
+        _thread.start_new_thread(thread_func, (args.endpoint, args.username, args.password, args.database, max_id, query_iterations))
+
+# Loop indefinitely to prevent application exit
+while 1:
+        pass
