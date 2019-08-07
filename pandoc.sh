fd1b8b558edc4e1de235dd56179b975d61dcf667
vladvataws <vladv@38f9d33824c1.ant.amazon.com>
2019-08-07 16:54:32 -0700

    Build steps for docx via pandoc

---
+++ pandoc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cp docs/assets/images/* temp/pandoc
+pandoc -f markdown -t html docs/index.md > temp/pandoc/output.html
+pandoc -f markdown -t html docs/modules/index.md >> temp/pandoc/output.html
+
+for MODULE in prerequisites create connect clone backtrack perf-insights
+do
+cp docs/modules/$MODULE/*.png temp/pandoc/ && pandoc -f markdown -t html docs/modules/$MODULE/index.md >> temp/pandoc/output.html
+done
+
+pandoc -f markdown -t html docs/related/labs/index.md >> temp/pandoc/output.html
+pandoc -f markdown -t html docs/contribute.md >> temp/pandoc/output.html
+pandoc -f markdown -t html docs/license.md >> temp/pandoc/output.html
