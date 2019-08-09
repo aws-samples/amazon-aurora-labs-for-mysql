57909feb24fa652d2247bd2ac21eae9770f3e070
vladvataws <vladv@38f9d33824c1.ant.amazon.com>
2019-08-08 21:59:54 -0700

    Added create serverless cluster lab to pandoc

--- pandoc.sh
+++ pandoc.sh
@@ -4,7 +4,7 @@ cp docs/assets/images/* temp/pandoc
 pandoc -f markdown -t html docs/index.md > temp/pandoc/output.html
 pandoc -f markdown -t html docs/modules/index.md >> temp/pandoc/output.html
 
-for MODULE in prerequisites create connect clone backtrack perf-insights
+for MODULE in prerequisites create connect clone backtrack perf-insights create-serverless
 do
 cp docs/modules/$MODULE/*.png temp/pandoc/ && pandoc -f markdown -t html docs/modules/$MODULE/index.md >> temp/pandoc/output.html
 done
